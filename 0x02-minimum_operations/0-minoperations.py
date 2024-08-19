4870e9143b0a0b7a9a2dcf5312d4bb5e33b4b200
Mai2aa <mysaadil0@gmail.com>
2024-08-19 17:55:36 +0300

    Minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''Calculates the number of operations'''
+
+
+def minOperations(n: int) -> int:
+    '''Minimum operation'''
+    if not isinstance(n, int) or n < 2:
+        return 0
+
+    prev = 1
+    operations = cur = value = 2
+    while n > value:
+        reminder = n - value
+        if reminder % cur == 0:
+            operations += 2
+            prev = cur
+            value += cur
+            cur = value
+        elif reminder % prev == 0:
+            operations += 1
+            value += prev
+            cur = value
+    return operations
