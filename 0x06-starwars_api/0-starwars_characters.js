2e290fffac4a06d442d86623afae45b884eaad02
Mai2aa <mysaadil0@gmail.com>
2024-09-06 04:20:02 +0300

    starwars api

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,40 @@
 #!/usr/bin/node
 const util = require('util');
 const request = util.promisify(require('request'));
-const filmID = process.argv[2];
+const movieId = process.argv[2];
 
-async function starwarsCharacters (filmId) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
-  let response = await (await request(endpoint)).body;
-  response = JSON.parse(response);
-  const characters = response.characters;
-
-  for (let i = 0; i < characters.length; i++) {
-    const urlCharacter = characters[i];
-    let character = await (await request(urlCharacter)).body;
-    character = JSON.parse(character);
-    console.log(character.name);
-  }
+if (!movieId) {
+    console.log("Usage: node starWarsCharacters.js <Movie ID>");
+    process.exit(1);
 }
 
-starwarsCharacters(filmID);
+async function getStarWarsCharacters(movieId) {
+    try {
+        // Fetch the film details from the Star Wars API
+        const filmResponse = await fetch(`https://swapi.dev/api/films/${movieId}/`);
+        if (!filmResponse.ok) {
+            throw new Error(`Error fetching film data: ${filmResponse.statusText}`);
+        }
+
+        const filmData = await filmResponse.json();
+
+        // Extract the characters list
+        const characters = filmData.characters;
+
+        // Print each character's name
+        for (const characterUrl of characters) {
+            const characterResponse = await fetch(characterUrl);
+            if (!characterResponse.ok) {
+                throw new Error(`Error fetching character data: ${characterResponse.statusText}`);
+            }
+
+            const characterData = await characterResponse.json();
+            console.log(characterData.name);
+        }
+
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+getStarWarsCharacters(movieId);
